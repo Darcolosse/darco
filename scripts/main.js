1ef189bf7ad9f96b5db8d293c024e63f735292c6
darcolosse <cgoues@gmail.com>
2024-09-28 17:03:25 +0200

    sqdfghj

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+function navbar(){
+    document.addEventListener('DOMContentLoaded', function () {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar').innerHTML = data;
+            })
+            .catch(error => console.error('Erreur de chargement de la navbar:', error));
+    });
+}
+
+function footer(){
+    document.addEventListener('DOMContentLoaded', function () {
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer').innerHTML = data;
+            })
+            .catch(error => console.error('Erreur de chargement du footer:', error));
+    });
+}
+
+function header(){
+    document.addEventListener('DOMContentLoaded', function () {
+        fetch('header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header').innerHTML = data;
+            })
+            .catch(error => console.error('Erreur de chargement du header:', error));
+    });
+}
+
+navbar();
+footer();
+header();
